8e50f9feeca2761821c5a80ed32db7005439c782
Hanya <hanya.runo@gmail.com>
2012-01-26 18:41:10 +0900

    exclude mri.xml from package

--- mkzip.sh
+++ mkzip.sh
@@ -4,7 +4,7 @@ NAME=MRI
 VERSION=1.1.0
 
 zip -9 -r -n pyc -o ${NAME}-${VERSION}.oxt \
- META-INF/* *.xcu *.xcs *.py *.xml MRILib/* \
+ META-INF/* *.xcu *.xcs *.py description.xml MRILib/* \
  descriptions/* dialogs/* help/* icons/* \
  notices/* web/* pythonpath/mytools_Mri/*.py \
  pythonpath/mytools_Mri/generators/*.py \
