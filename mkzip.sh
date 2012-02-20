ff9f2e580805c21e8d7c2c392bedc2daf150fa48
Tsutomu Uchino <hanya.runo@gmail.com>
2012-02-21 02:59:03 +0900

    prepared for 1.1.1

--- mkzip.sh
+++ mkzip.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 NAME=MRI
-VERSION=1.1.0
+VERSION=1.1.1
 
 zip -9 -r -n pyc -o ${NAME}-${VERSION}.oxt \
- META-INF/* *.xcu *.xcs *.py *.xml MRILib/* \
+ META-INF/* *.xcu *.xcs *.py description.xml MRILib/* \
  descriptions/* dialogs/* help/* icons/* \
  notices/* web/* pythonpath/mytools_Mri/*.py \
  pythonpath/mytools_Mri/generators/*.py \
