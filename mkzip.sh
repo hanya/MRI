9bc90a8f07c939eea809c414b49412af3b648ec8
Tsutomu Uchino <hanya.runo@gmail.com>
2013-01-12 02:36:47 +0900

    Fix remove genres.py from package

--- mkzip.sh
+++ mkzip.sh
@@ -4,7 +4,7 @@ NAME=MRI
 VERSION=`cat "VERSION"`
 
 zip -9 -r -n pyc -o files/${NAME}-${VERSION}.oxt \
- META-INF/* *.xcu *.xcs *.py description.xml MRILib/* \
+ META-INF/* *.xcu *.xcs MRI.py description.xml MRILib/* \
  descriptions/* dialogs/* help/* icons/* \
  notices/* web/* pythonpath/mytools_Mri/*.py \
  pythonpath/mytools_Mri/generators/*.py \
